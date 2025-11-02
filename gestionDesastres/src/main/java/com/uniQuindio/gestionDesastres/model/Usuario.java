54ee7bf02a3bbf7279da9e8753c57c7ebe3081c1
nikoll <nikolls.ocampog@uqvirtual.edu.co>
2025-11-02 15:24:02 -0500

    feat: agregar clase usuario

--- gestionDesastres/src/main/java/com/uniQuindio/gestionDesastres/model/Usuario.java
+++ gestionDesastres/src/main/java/com/uniQuindio/gestionDesastres/model/Usuario.java
@@ -1,4 +1,52 @@
 package com.uniQuindio.gestionDesastres.model;
 
-public class Usuario {
+public abstract class Usuario {
+
+    protected String nombre;
+    protected String id;
+    protected String email;
+    protected String contrasena;
+
+
+    public Usuario(String nombre, String id, String email, String contrasena) {
+        this.nombre = nombre;
+        this.id = id;
+        this.email = email;
+        this.contrasena = contrasena;
+    }
+    public Usuario() {
+    }
+
+    // Métodos comunes
+    public boolean iniciarSesion(String email, String contrasena) {
+        return this.email.equals(email) && this.contrasena.equals(contrasena);
+    }
+
+    public void cerrarSesion() {
+        System.out.println(nombre + " ha cerrado sesión.");
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    public void getContrasena() {
+        return;
+    }
 }
